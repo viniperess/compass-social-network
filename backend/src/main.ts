beeab2e265f02c3a8a8c240e35b23c34936e64c4
Vinicius Peres <viniciusgperes@hotmail.com>
2023-05-27 11:33:19 -0300

    feat: cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as cors from 'cors';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-
+  // Enable CORS
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  app.use(cors(corsOptions));
   app.setGlobalPrefix('/api/v1/');
 
   await app.listen(3001);
